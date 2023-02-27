11fda358db3eba93e8d8c0317e494c1818014b4c
boogie <1269594354@qq.com>
2023-02-27 19:01:03 +0800

    First commit

---
+++ src/main/kotlin/com/example/xiaohongshuall/XiaoHongShuAllApplication.kt
@@ -0,0 +1,11 @@
+package com.example.xiaohongshuall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XiaoHongShuAllApplication
+
+fun main(args: Array<String>) {
+    runApplication<XiaoHongShuAllApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/xiaohongshuall/XiaoHongShuAllApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.xiaohongshuall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XiaoHongShuAllApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
